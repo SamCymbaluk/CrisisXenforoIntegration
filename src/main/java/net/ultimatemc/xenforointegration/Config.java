f049c87973143847c8d4478117e9741ceb6bacd5
Sam Cymbaluk <samcymbaluk@gmail.com>
2017-02-25 22:04:48 -0500

    Added config file

--- src/main/java/net/ultimatemc/xenforointegration/Config.java
+++ src/main/java/net/ultimatemc/xenforointegration/Config.java
@@ -2,35 +2,70 @@ package net.ultimatemc.xenforointegration;
 
 public class Config {
 
-    private Database database;
+    private Database database = new Database(
+            "jdbc:mysql://localhost:3306/database",
+            "user",
+            "password"
+    );
+
+    private Website website = new Website(
+            "https://mysite.com/api.php",
+            "my_api_key"
+    );
+
+
 
     public Database getDatabase() {
         return database;
     }
+
+    public Website getWebsite() {
+        return website;
+    }
+
+    class Database {
+
+        private String host;
+        private String user;
+        private String password;
+
+        public Database(String host, String user, String password) {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    class Website {
+
+        private String apiUrl;
+        private String apiKey;
+
+        public Website(String apiUrl, String apiKey) {
+            this.apiUrl = apiUrl;
+            this.apiKey = apiKey;
+        }
+
+        public String getApiUrl() {
+            return apiUrl;
+        }
+
+        public String getApiKey() {
+            return apiKey;
+        }
+    }
 }
 
 
-class Database {
-
-    private String host;
-    private String user;
-    private String password;
-
-    public Database(String host, String user, String password) {
-        this.host = host;
-        this.user = user;
-        this.password = password;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-}
--- src/main/java/net/ultimatemc/xenforointegration/XenforoIntegration.java
+++ src/main/java/net/ultimatemc/xenforointegration/XenforoIntegration.java
@@ -1,13 +1,15 @@
 package net.ultimatemc.xenforointegration;
 
 import commands.RegisterCommand;
-import commands.WebCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import storage.SqlDatabase;
 import storage.UserLoader;
+import util.ConfigLoader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,16 +19,28 @@ public class XenforoIntegration extends JavaPlugin {
     private Map<String, Integer> users = new ConcurrentHashMap<>();
     public XenApiWrapper xenApi;
     private UserLoader userLoader;
+    private Config config;
 
     @Override
     public void onEnable() {
+        try {
+            File dataFolder = this.getDataFolder();
+            dataFolder.mkdir();
+            config = (Config) ConfigLoader.loadConfig(new Config(), new File(this.getDataFolder().getPath() + File.separator + "config.json"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.err.println("Error loading XenforoIntegration config");
+            return;
+        }
+        db = new SqlDatabase(this, config.getDatabase().getHost(), config.getDatabase().getUser(), config.getDatabase().getPassword());
         //db = new SqlDatabase(this, "jdbc:mysql://ultimatemc.net:3306/web", "xenforo", "QJMTEz1ulJRwtqyuQYkb"); //Remote
-        db = new SqlDatabase(this, "jdbc:mysql://localhost:3306/xenforo", "root", "e262Y%39f0d7e956642#0((47d19fc9e94c"); //Local
-        xenApi = new XenApiWrapper("https://ultimatemc.net/api.php", "6f78c25a93b4c327846ce0f50c5e675ba35d271518471d8228c0850ecd3f5aab");
+        //db = new SqlDatabase(this, "jdbc:mysql://localhost:3306/xenforo", "root", "e262Y%39f0d7e956642#0((47d19fc9e94c"); //Local
+        //xenApi = new XenApiWrapper("https://ultimatemc.net/api.php", "6f78c25a93b4c327846ce0f50c5e675ba35d271518471d8228c0850ecd3f5aab");
+        xenApi = new XenApiWrapper(config.getWebsite().getApiUrl(), config.getWebsite().getApiKey());
         userLoader = new UserLoader(this, db);
 
         this.getCommand("register").setExecutor(new RegisterCommand(this, userLoader));
-        this.getCommand("web").setExecutor(new WebCommand(this, userLoader));
+        //this.getCommand("web").setExecutor(new WebCommand(this, userLoader));
 
         for (Player p : Bukkit.getOnlinePlayers()) {
             userLoader.loadUser(p.getUniqueId().toString());
